e4ace6ca548ed8279dbb489f136371e06e859340
otavio <otavmacedo04@gmail.com>
2026-02-02 19:55:29 -0300

    add type hint for get_model function

--- src/agent/model.py
+++ src/agent/model.py
@@ -1,4 +1,4 @@
-from langchain.chat_models import init_chat_model
+from langchain.chat_models import init_chat_model, BaseChatModel
 
-def get_model(temperature=0):
+def get_model(temperature=0) -> BaseChatModel:
     return init_chat_model(model='openai:gpt-5-mini', temperature=temperature)
